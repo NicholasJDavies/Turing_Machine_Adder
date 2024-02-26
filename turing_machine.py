4fbc4c9ce48099d25109f7e1eb414b65ba8435c1
Nicholas Davies <nicholasjohndavies42@gmail.com>
2024-02-26 12:56:05 +0800

    first commit

---
+++ turing_machine.py
@@ -0,0 +1,14 @@
+# Author: Nicholas Davies
+
+class machine:
+    '''     Symbol----
+    dial:   __  X   1
+    1       D6  E2  R1
+    2       R2  E3  ? 
+    3       R3  E4  E5
+    4       L4  ?   R6
+    5       L5  ?   R1
+    6       X6  !   R3
+    '''
+    
+    position = 0
